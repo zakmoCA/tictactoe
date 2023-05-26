6bb82acd81d951dacb97b3f885ffd890fee1bea5
zakmoCA <13905@coderacademy.edu.au>
2023-05-26 23:20:05 +1000

    extracted win and draw logic into seperate functions, updated checking for draw logic

--- script.js
+++ script.js
@@ -30,63 +30,67 @@ const winningCombos = [
   [3, 5, 7]
 ]
 
+// check for draw condition
+function checkForDraw() {
+  return winningCombos.every(combo => {
+    let comboValues = combo.map(id => gameBoard[id]);
+    return comboValues.includes('X') && comboValues.includes('O');
+  });
+}
+
+// checks for win condition
+function checkForWin() {
+  for (let i = 0; i < winningCombos.length; i++) {
+    let combo = winningCombos[i];
+    let comboValues = combo.map(id => gameBoard[id]);
+    if (comboValues.every(val => val === comboValues[0] && val !== '')) {
+      currentPlayer = comboValues[0]
+      console.log(`Player ${currentPlayer} is the winner!`);
+      const gameWinner = document.createElement('p')
+      gameWinner.setAttribute('id', 'game-winner')
+      declaredWinner.appendChild(gameWinner)
+      gameWinner.innerText = `Player ${currentPlayer} is the winner!`
+      return true;
+    }
+  }
+  return false;
+}
 
 function game() {
   cells.forEach(cell => {
     cell.addEventListener('click', (e) => {
+      if (gameOver) return;
       const elementId = e.target.id
-      console.log(e.target.id)
-      // If the cell has already been clicked, stop the function.
-      if (clickedCells.includes(e.target.id)) {
+      if (clickedCells.includes(elementId)) {
         return;
       }
-      if (gameOver) return
-      if (!clickedCells.includes(e.target.id) && currentPlayer === '') {
-        clickedCells.push(e.target.id)
-        e.target.innerText = 'X'
-        gameBoard[elementId] = 'X'
-        currentPlayer = 'circle'
-      } else if (!clickedCells.includes(e.target.id) && currentPlayer === 'circle') {
-        clickedCells.push(e.target.id)
+
+      clickedCells.push(elementId)
+
+      if (currentPlayer === 'circle') {
         e.target.innerText = 'O'
         gameBoard[elementId] = 'O'
         currentPlayer = 'x'
-      } else if (!clickedCells.includes(e.target.id) && currentPlayer === 'x') {
-        clickedCells.push(e.target.id)
+      } else {
         e.target.innerText = 'X'
         gameBoard[elementId] = 'X'
         currentPlayer = 'circle'
       }
-      console.log(clickedCells)
-      console.log(gameBoard)
 
-      if (clickedCells.length >= 5) {
-        for (let i = 0; i < winningCombos.length; i++) {
-          let combo = winningCombos[i];
-          let comboValues = combo.map(id => gameBoard[id]);
-          if (comboValues.every(val => val === comboValues[0] && val !== '')) {
-            currentPlayer = comboValues[0]
-            console.log(`Player ${currentPlayer} is the winner!`);
-            const gameWinner = document.createElement('p')
-            gameWinner.setAttribute('id', 'game-winner')
-            declaredWinner.appendChild(gameWinner)
-            gameWinner.innerText = `Player ${currentPlayer} is the winner!`
-            gameOver = true
-          }
-        }
-      } 
-      // basic draw criteria of game ended without winner
-      if (clickedCells.length >= 9 && !gameOver) {
+      if (checkForWin()) {
+        gameOver = true;
+        return;
+      }
+
+      if (checkForDraw()) {
         const draw = document.createElement('p')
         draw.setAttribute('id', 'game-draw')
         declareDraw.appendChild(draw)
         declareDraw.innerText = `The game ends in a draw!`
+        gameOver = true;
+        return;
       }
-
-      // draw criteria if no winning combos left
-      
     })
   })
 }
-
 game()
